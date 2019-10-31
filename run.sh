e06d9b2488a61b1577df87de1980106ff2edf4f3
shmsw25 <sewon@cs.washington.edu>
2019-10-31 12:49:46 -0700

    add verbose in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 data=$1
 loss=$2
 bs=48
-pbs=96
+pbs=600
 data_dir="preprocessed-open-domain-qa-data"
 
 if [ ${loss} = "hard-em" ]
@@ -22,7 +22,7 @@ test_file="${data_dir}/${data}-test.json"
 
 python3 main.py --do_train --output_dir ${output_dir} \
           --train_file ${train_file} --predict_file ${dev_file} \
-          --train_batch_size ${bs} --predict_batch_size ${pbs} --loss_type ${loss} --tau ${tau}
+          --train_batch_size ${bs} --predict_batch_size ${pbs} --verbose --loss_type ${loss} --tau ${tau}
 python3 main.py --do_predict --output_dir ${output_dir} \
           --predict_file ${dev_file} \
           --init_checkpoint ${output_dir}/best-model.pt \
